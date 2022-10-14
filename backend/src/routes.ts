ebba03ce792e9b558591a948e2131e26a2a14bca
thiagobrsouza-edu <thiago.glespaul@gmail.com>
2022-10-14 15:47:50 -0300

    Create routes

---
+++ backend/src/routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { ProfileController } from "./controllers/ProfileController";
+import { UserController } from "./controllers/UserController";
+
+export const routes = Router()
+
+/**
+ * profiles routes
+ */
+routes.get('/profiles', new ProfileController().findAll)
+routes.get('/profiles/:id', new ProfileController().findOne)
+
+/**
+ * users routes
+ */
+routes.post('/users', new UserController().create)
+routes.get('/users', new UserController().findAll)
+routes.get('/users/:id', new UserController().findOne)
+routes.patch('/users/:id', new UserController().update)
+routes.delete('/users/:id', new UserController().remove)
